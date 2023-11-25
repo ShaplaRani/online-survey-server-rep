15cba5e802b974707b286bfb5f252fe12d082155
ShaplaRani <shaplarani621@gmail.com>
2023-11-25 11:11:14 +0600

    create the token and  admin route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express =  require('express')
+const express = require('express')
 require('dotenv').config()
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,6 +29,60 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     //await client.connect();
     const userCollection = client.db("surveyDb").collection("users");
+
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '8h' });
+      console.log(token);
+      res.send({ token });
+    })
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      console.log(user);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+    //check admin
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
     //user create
     app.post('/users', async (req, res) => {
       const user = req.body;
@@ -52,9 +107,9 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/' , (req, res) => {
-    res.send('survey is sitting')
+app.get('/', (req, res) => {
+  res.send('survey is sitting')
 })
-app.listen(port , () => {
-    console.log(`survey is sitting on port:${port}`);
+app.listen(port, () => {
+  console.log(`survey is sitting on port:${port}`);
 })
